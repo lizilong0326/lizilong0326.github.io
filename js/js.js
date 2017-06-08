7c99ddba4fc7dce0123fb99e0cf9158ba97a4309
lizilong0326 <792979941@qq.com>
2017-06-08 20:26:25 +0800

    one

---
+++ js/js.js
@@ -0,0 +1,84 @@
+var app = angular.module("myApp", ['ui.router']);
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: "../views/home.html"
+        }).state('work', {
+            url: '/work',
+            templateUrl: "../views/work.html",
+            controller: "findCtrl"
+        }).state('about', {
+            url: '/about',
+            templateUrl: "../views/about.html"
+        }).state('contenct', {
+            url: '/contenct',
+            templateUrl: "../views/contenct.html"
+        })
+
+
+
+}]);
+app.directive("textclose", function () {
+        return {
+            restrict: "C",
+            link:function(scope, element, attrs){
+            	$(".dts dt").hover(function(){
+            		$(this).find(".hide").stop().animate({opacity:"1"});
+            	},function(){
+            		$(this).find(".hide").stop().animate({opacity:"0"});
+            	})
+            }
+
+        }
+
+    }).directive("bodyhide", function () {
+        return {
+            restrict: "C",
+            link:function(scope, element, attrs){
+            	setTimeout(function(){
+            		$(".loding").stop().animate({opacity:"0"});
+            		$(".wrap").stop().animate({opacity:"1"},2000);
+            	},3000)
+            }
+
+        }
+
+    })
+app.controller('findCtrl', function ($scope) {
+    $scope.init = function (name, h) {
+    	 document.documentElement.scrollTop = document.body.scrollTop =0;
+        var stt = '<div class="addbox"><span class="remove"></span></div>';
+        $("body").append(stt);
+        var str = '<div class="addimgs"><img class = "addimg" src = "../imgs/' + name + '" alt = "" ></div>';
+        $("body").append(str);hn
+
+        $(".addbox").height(h)
+        $(".remove").on("click", function () {
+            $(".addbox").remove();
+            $(".addimgs").remove();
+        })
+        $(".addbox").on("click", function () {
+            $(".addbox").remove();
+            $(".addimgs").remove();
+        })
+    }
+    $scope.inits = function (h) {
+    	 document.documentElement.scrollTop = document.body.scrollTop =0;
+        var stt = '<div class="addbox"><span class="remove"></span></div>';
+        $("body").append(stt);
+        var str = '<div class="addimgs" style="background:#FFFFFF"><img src = "../imgs/1.jpg" alt = "" ><img src = "../imgs/gif4.gif" alt = "" ><img src = "../imgs/gif1.gif" alt = "" ><img  src = "../imgs/gif2.gif" alt = "" ><img src = "../imgs/gif3.gif" alt = "" ><img src = "../imgs/3.jpg" alt = "" ></div>';
+        $("body").append(str);
+        $(".addbox").height(h)
+        $(".remove").on("click", function () {
+            $(".addbox").remove();
+            $(".addimgs").remove();
+        })
+        $(".addbox").on("click", function () {
+            $(".addbox").remove();
+            $(".addimgs").remove();
+        })
+    }
+})
